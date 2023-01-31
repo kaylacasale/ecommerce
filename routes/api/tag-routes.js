2c1fa0b449cb16ebdbcc6be9c80f3babc7313126
Kayla Casale <kayla.casale@gmail.com>
2023-01-30 22:39:06 -0800

    added parameter info to models and relationship data in index

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tage_name',
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stocks',
+          'category_id',
+        ]
+      }
+    ]
+  })
+    .then(tagData => {
+      console.log(tagData)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
