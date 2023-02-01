a3c131819e99a2983b9ca701b6ddd842d7e35c30
Kayla Casale <kayla.casale@gmail.com>
2023-01-31 16:38:42 -0800

    added function to create new category with post request in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,6 +50,23 @@ router.get('/', (req, res) => {
 
 });
 
+//* Postman request URL (example): 'http://localhost:3001/api/categories/4'
+//* Postman response: 
+// {
+//   "id": 4,
+//   "category_name": "Hats",
+//   "products": [
+//       {
+//           "id": 3,
+//           "product_name": "Branded Baseball Hat",
+//           "price": 23,
+//           "category_id": 4
+//       }
+//   ]
+// }
+//* console.log (example): find category with id= 4 by primary key in category-routes
+
+//* GET route to find a category by primary key
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   Category.findByPk(req.params.id, {
@@ -79,8 +96,36 @@ router.get('/:id', (req, res) => {
 
 });
 
+//* SQL syntax for inserting new cateogory upon POST request: 
+//Executing (default): INSERT INTO `category` (`id`,`category_name`) VALUES (DEFAULT,?);
+// category {
+//   dataValues: { id: 6, category_name: 'Hoodies' },
+
+//* Postman POST request URL (example): 'http://localhost:3001/api/categories'
+//* Postman JSON request body (example): 
+// {
+//   "category_name": "Hoodies"
+//   }
+//* Postman POST request response:
+// {
+//   "id": 6,
+//   "category_name": "Hoodies"
+// }
+//* POST route to add new category to database (only parameter available for input is 'category_name' since id is autoincrement from prior category ids)
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    ...req.body,
+    category_name: req.body.category_name,
+  })
+    .then((newCategory) => {
+      res.status(200).json(newCategory);
+      console.log(newCategory, `new category called ${req.body.category_name} added to db!`)
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
