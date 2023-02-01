7fde0f34a3ce8e8bb53f7797aac2985af03b6f00
Kayla Casale <kayla.casale@gmail.com>
2023-01-31 22:14:13 -0800

    added commenting and console.logs to display data in responses via template literals

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,6 +4,51 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
+//* Postman GET request URL: 'http://localhost:3001/api/products'
+//* Postman GET response (one example object): 
+// [
+//   {  //* Product attributes
+//       "id": 1,
+//       "product_name": "Plain T-Shirt",
+//       "price": 14.99,
+//       "stock": 14,
+//       "category_id": 1,
+//       "tags": [
+//           { //* Tag attributes (x3)
+//               "id": 6,
+//               "tag_name": "white",
+//               "product_tag": { //* ProductTag attributes
+//                   "id": 1,
+//                   "product_id": 1,
+//                   "tag_id": 6
+//               }
+//           },
+//           {
+//               "id": 7,
+//               "tag_name": "gold",
+//               "product_tag": { //* ProductTag attributes
+//                   "id": 2,
+//                   "product_id": 1,
+//                   "tag_id": 7
+//               }
+//           },
+//           {
+//               "id": 8,
+//               "tag_name": "pop culture",
+//               "product_tag": { //* ProductTag attributes
+//                   "id": 3,
+//                   "product_id": 1,
+//                   "tag_id": 8
+//               }
+//           }
+//       ],
+//       "category": { //* Category attributes
+//           "id": 1,
+//           "category_name": "Shirts"
+//       }
+//   },
+//.....] //* example Product object returned
+//* GET request to find all products (inclduing tag and category models)
 router.get('/', (req, res) => {
   // find all products
   Product.findAll({
@@ -44,6 +89,35 @@ router.get('/', (req, res) => {
 });
 
 // get one product
+//* Postman GET request URL (example): 'http://localhost:3001/api/products/2'
+//* Postman GET response (example for id=2): 
+// {
+//   "id": 2,
+//   "product_name": "Running Sneakers",
+//   "price": 90,
+//   "stock": 25,
+//   "category_id": 5,
+//   "category": {
+//       "id": 5,
+//       "category_name": "Shoes"
+//   },
+//   "tags": [
+//       {
+//           "id": 6,
+//           "tag_name": "white",
+//           "product_tag": {
+//               "id": 4,
+//               "product_id": 2,
+//               "tag_id": 6
+//           }
+//       }
+//   ]
+// }
+//* SQL backend: 
+// SELECT `product`.`id`, `product`.`product_name`, `product`.`price`, `product`.`stock`, `product`.`category_id`, `category`.`id` AS `category.id`, `category`.`category_name` AS `category.category_name`, `tags`.`id` AS `tags.id`, `tags`.`tag_name` AS `tags.tag_name`, `tags->product_tag`.`id` AS `tags.product_tag.id`, `tags->product_tag`.`product_id` AS `tags.product_tag.product_id`, `tags->product_tag`.`tag_id` AS `tags.product_tag.tag_id` FROM `product` AS `product` LEFT OUTER JOIN `category` AS `category` ON `product`.`category_id` = `category`.`id` LEFT OUTER JOIN ( `product_tag` AS `tags->product_tag` INNER JOIN `tag` AS `tags` ON `tags`.`id` = `tags->product_tag`.`tag_id`) ON `product`.`id` = `tags->product_tag`.`product_id` WHERE `product`.`id` = '2';
+//* console.log response (example): 
+// find product data by primary key in product-routes where id = 2
+//* GET request to find one product by primary key (id)
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   Product.findByPk(req.params.id, {
@@ -81,7 +155,7 @@ router.get('/:id', (req, res) => {
         return;
       }
       res.json(productData);
-      console.log(productData, 'find product data by primary key in product-routes')
+      console.log(productData, `find product data by primary key in product-routes where id = ${req.params.id}`)
     })
     .catch(err => {
       console.log(err);
@@ -107,6 +181,7 @@ router.get('/:id', (req, res) => {
   }
 */
 
+//* Postman POST request URL: 'http://localhost:3001/api/products'
 // {
 //   product_name: "Toothbrush",
 //   price: 200.00,
@@ -127,11 +202,13 @@ router.get('/:id', (req, res) => {
 //   "stock": 5
 // }
 // }//* use async function with await to relieve promises
-//* returned on console: 
+//* SQL backend: 
 // INSERT INTO `product` (`id`,`product_name`,`price`,`stock`) VALUES (DEFAULT,?,?,?);
 // product {
 //   dataValues: { id: 6, product_name: 'Toothbrush', price: 200, stock: 5 }
-
+//* console.log response (example): 
+// new product called Toothbrush with a price of 200, stock equal to 5
+//* POST request to add new product with attributes: product_name, price, stock, and tagIds
 router.post('/', async (req, res) => {
   try {
     const newProduct = await Product.create({
@@ -153,7 +230,7 @@ router.post('/', async (req, res) => {
 
     }
     res.status(200).json(newProduct)
-    console.log(newProduct)
+    console.log(newProduct, `new product called ${req.body.product_name} with a price of ${req.body.price}, stock equal to ${req.body.stock}`)
 
   } catch (err) {
     res.status(400).json(err);
@@ -161,79 +238,7 @@ router.post('/', async (req, res) => {
 })
 
 
-//   Product.create({
-//     product_name: req.body.product_name,
-//     price: req.body.price,
-//     stock: req.body.stock,
-//     category_id: req.body.category_id,
-//     tagIds: req.body.tag_id
-//   })
-//     .then((product) => {
-//       //* map through tag_id's array 
-//       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-//       if (req.body.tagIds.length) {
-//         const productTagIdArr = req.body.tagIds.map((tag_id) => {
-//           return {
-//             product_id: product.id,
-//             tag_id,
-//           };
-//         });
-//         //* ProductTag model is a through table with 'product_id' and 'tag_id'
-//         return ProductTag.bulkCreate(productTagIdArr);
-//       }
-//       // if no product tags, just respond
-//       res.status(200).json(product);
-//     })
-//     .then((productTagIds) => res.status(200).json(productTagIds))
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(400).json(err);
-//     });
-// });
 
-// update product
-// router.put('/:id', (req, res) => {
-//   // update product data
-//   Product.update(req.body, {
-//     where: {
-//       id: req.params.id,
-//     },
-//   })
-//     .then((product) => {
-//       // find all associated tags from ProductTag
-//       // console.log(product)
-//       return ProductTag.findAll({ where: { product_id: req.params.id } });
-//     })
-//     .then((productTags) => {
-//       // get list of current tag_ids
-//       const productTagIds = productTags.map((tag_id) =>
-//         tag_id.get({ plain: true }));
-//       // create filtered list of new tag_ids
-//       const newProductTags = req.body.tagIds
-//         .filter((tag_id) => !productTagIds.includes(tag_id))
-//         .map((tag_id) => {
-//           return {
-//             product_id: req.params.id,
-//             tag_id,
-//           };
-//         });
-//       // figure out which ones to remove
-//       const productTagsToRemove = productTags
-//         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-//         .map(({ id }) => id);
-
-//       // run both actions
-//       return Promise.all([
-//         ProductTag.destroy({ where: { id: productTagsToRemove } }),
-//         ProductTag.bulkCreate(newProductTags),
-//       ]);
-//     })
-//     .then((updatedProductTags) => res.json(updatedProductTags))
-//     .catch((err) => {
-//       // console.log(err);
-//       res.status(400).json(err);
-//     });
-// });
 //* Postman URL example: 'http://localhost:3001/api/products/4' 
 //* Postman JSON Body entry:
 // {
@@ -260,6 +265,7 @@ router.put('/:id', (req, res) => {
     })
     .then((productTags) => {
       // get list of current tag_ids
+      //* ProductTag model is a through table with 'product_id' and 'tag_id'
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,7 +2,41 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+//* Postman GET URL: 'http://localhost:3001/api/tags'
+//* response: array of objects showing tags and products with same tag_id
+//[
+// {
+//     "id": 1,
+//     "tag_name": "rock music",
+//     "products": [
+//         {
+//             "id": 3,
+//             "product_name": "Branded Baseball Hat",
+//             "price": 22.99,
+//             "stock": 12,
+//             "category_id": 4,
+//             "product_tag": {
+//                 "id": 5,
+//                 "product_id": 3,
+//                 "tag_id": 1
+//             }
+//         },
+//         {
+//             "id": 4,
+//             "product_name": "Top 40 Music Compilation Vinyl Record",
+//             "price": 12.99,
+//             "stock": 50,
+//             "category_id": 3,
+//             "product_tag": {
+//                 "id": 9,
+//                 "product_id": 4,
+//                 "tag_id": 1
+//             }
+//         }
+//     ]
+// },
+// ].... //* ^ example of 1 tag object
+//* GET route to find all tags (including products associated)
 router.get('/', (req, res) => {
   Tag.findAll({
     attributes: [
@@ -34,10 +68,31 @@ router.get('/', (req, res) => {
   // be sure to include its associated Product data
 });
 
+//* Postman GET request URL (example): 'http://localhost:3001/api/tags/2'
+//* response: a tag object (with a product, can have many)
+// {
+//   "id": 2,
+//   "tag_name": "pop music",
+//   "products": [
+//       {
+//           "id": 4,
+//           "product_name": "Top 40 Music Compilation Vinyl Record",
+//           "price": 12.99,
+//           "stock": 50,
+//           "category_id": 3,
+//           "product_tag": {
+//               "id": 10,
+//               "product_id": 4,
+//               "tag_id": 2
+//           }
+//       }
+//   ]
+// }
+//* GET route to find one tag (and associated products with same tag_id) by primary key (id)
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  //* find rag by primary key (include attributes listed and product model with its own attributes listed)
+  //* find tag by primary key (include attributes listed and product model with its own attributes listed)
   Tag.findByPk(req.params.id, {
     attributes: [
       'id',
@@ -68,19 +123,32 @@ router.get('/:id', (req, res) => {
     })
 });
 
+//* in order to do update route below, specify data input being sent (tag_name)
+//* Postman route: 'http://localhost:3001/api/tags/'
+//* enter object with 'tag_name':
+//* Postman body example entry: {"tag_name": hip hop music}
+//   {
+//     "tag_name": "classical music"
+// }
+//* response with new object added to tags array of objects (given new id):
+//   {
+//     "id": 9,
+//     "tag_name": "classical music"
+// }
+//* console.log response: 
+//  new tag called country music added to the db!
+// 'country music' = template literal that displays the name of new tag in console upon successful post request
+//* send to post route to add new tag to db
+//* Postman response example: {"id": 9}
+//* POST request to add new tag (input new tag_name) to the db
 router.post('/', (req, res) => {
   // create a new tag
-  //* in order to do update route below, specify data input being sent (tag_name)
-  //* Postman route: 'http://localhost:3001/api/tags/'
-  //* enter object with 'tag_name':
-  //* Postman body example entry: {"tag_name": hip hop music}
-  //* send to post route to add new tag to db
-  //* Postman response example: {"id": 9}
   Tag.create({
     tag_name: req.body.tag_name,
   })
     .then(newTagData => {
       res.status(200).json(newTagData)
+      console.log(newTagData, `new tag called ${req.body.tag_name} added to the db!`)
     })
     .catch((err) => {
       console.log(err)
@@ -88,6 +156,20 @@ router.post('/', (req, res) => {
     })
 });
 
+//* Postman PUT request URL (example): 'http://localhost:3001/api/tags/2'
+//* body entry (example): 
+// {
+//   "tag_name": "dance music"
+// }
+//* Postman response: 
+// [
+//   1
+// ]
+//* SQL backend: 
+// UPDATE `tag` SET `tag_name`=? WHERE `id` = ?
+//* console.log response: 
+// [ 1 ] updated tag_name  through tag-routes
+//* PUT request to update tag data (tag_name) in tag-routes
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update(req.body, {
@@ -106,7 +188,7 @@ router.put('/:id', (req, res) => {
         return;
       }
       res.json(updateTagData)
-      console.log(updateTagData, 'updated tag_name  through tag-routes')
+      console.log(updateTagData, `updated tag_name  through tag-routes`)
 
     })
     .catch(err => {
@@ -115,9 +197,40 @@ router.put('/:id', (req, res) => {
     })
 });
 
-//* Postman url example: 'http://localhost:3001/api/tags/3'
+//* 3 steps to verify deletion:
+//* 1) GET request to find tag by id: 'http://localhost:3001/api/tags/2'
+// {
+//   "id": 2,
+//   "tag_name": "pop music",
+//   "products": [
+//       {
+//           "id": 4,
+//           "product_name": "Top 40 Music Compilation Vinyl Record",
+//           "price": 12.99,
+//           "stock": 50,
+//           "category_id": 3,
+//           "product_tag": {
+//               "id": 10,
+//               "product_id": 4,
+//               "tag_id": 2
+//           }
+//       }
+//   ]
+// }
+//* 2) DELETE request to delete tag object by id (mentioned in delete request url)
+//* Postman response: '1'
+//* SQL backend: 
+//  DELETE FROM `tag` WHERE `id` = '2'
+//* console.log response: 
+// 1 tag_name deleted from tags with id= 2
+//* 3) GET request with same id to verify no existing object after deletion: 'http://localhost:3001/api/tags/2'
+//* verification response: 'null'
+
+//* Postman DELETE request url (example): 'http://localhost:3001/api/tags/3'
 //* will delete tag_name with id=3 and return '1'
 //* DELETE FROM `tag` WHERE `id` = '5'
+
+//* DELETE request to delete tag object by id
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
   Tag.destroy({
